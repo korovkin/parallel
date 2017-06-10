d13c3e9d4df653e5949d7fc6b8591ef9a4314170
Haim Grosman <korovkin@gmail.com>
2017-06-10 12:26:56 -0700

    thrift interface for the master/slave RPC

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+build:
+	thrift -r --gen go parallel.thrift
+	@mv gen-go/parallel/constants.go parallel_constants.go
+	@mv gen-go/parallel/parallel.go parallel_service.go
+	@mv gen-go/parallel/ttypes.go parallel_types.go
+	@rm -rf gen-go
+
---
+++ parallel.thrift
@@ -0,0 +1,18 @@
+enum Result {
+  OK = 0,
+  ERROR = 2,
+}
+
+struct Cmd {
+  1: string cmdLine = "";
+  2: i64 ticket = 0;
+}
+
+exception ExecuteException {
+  1: string what;
+  2: string output;
+}
+
+service Parallel {
+   string Execute(1:Cmd command) throws (1:ExecuteException e),
+}
